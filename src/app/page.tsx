2b966b58d923ee30956299c9d347d7b5419c84a6
Nihindu Dulavin <nihindudulavin02@gmail.com>
2026-07-06 22:27:58 +0530

    Configure Tailwind CSS v4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           priority
         />
         <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
+          <h1 className="text-3xl font-extrabold text-blue-600 tracking-tight mb-4">To get started, edit the page.tsx file.</h1>
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
             <a
